4a8ac99c9a34004b890f67b1a9630fd27180429d
Jury Razumau <2369296+razumau@users.noreply.github.com>
2022-09-24 22:48:01 +0200

    build basic redirect flow

--- main.go
+++ main.go
@@ -1,39 +1,53 @@
 package main
 
 import (
-	"html/template"
 	"io"
 	"log"
 	"net/http"
+	"text/template"
 	"time"
 
 	"github.com/gorilla/mux"
 )
 
+var comparisonTemplate = template.Must(template.ParseFiles("templates/comparison.html"))
+
 type ComparisonPageData struct {
-	PageTitle      string
+	ComparisonId   string
 	FirstQuestion  string
 	SecondQuestion string
 }
 
-func aboutHandler(w http.ResponseWriter, req *http.Request) {
+func buildNextId() string {
+	return "/15"
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "<html><body><h1>About this</h1></body></html>")
 }
 
+func selectionHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	comparison_id := vars["id"]
+	selection := vars["selection"]
+	log.Printf("Saving that for %s the %s question has been selected", comparison_id, selection)
+	http.Redirect(w, r, buildNextId(), http.StatusFound)
+}
+
 func comparisonHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/comparison.html"))
 	vars := mux.Vars(r)
 	data := ComparisonPageData{
-		PageTitle:      vars["id"],
+		ComparisonId:   vars["id"],
 		FirstQuestion:  "Первый вопрос",
 		SecondQuestion: "Второй вопрос",
 	}
-	tmpl.Execute(w, data)
+	comparisonTemplate.Execute(w, data)
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/about", aboutHandler)
+	r.Path("/submission/{id}").Queries("selection", "{selection}").HandlerFunc(selectionHandler)
 	r.HandleFunc("/{id}", comparisonHandler)
 	srv := &http.Server{
 		Handler:      r,
